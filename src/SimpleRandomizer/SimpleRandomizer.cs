999682556ac0333e4bb70ed6431ef69f250fb84b
Xilosof <pro100temi444@gmail.com>
2021-02-20 23:07:47 +0300

    Added first method for getting rand int

--- src/SimpleRandomizer/SimpleRandomizer.cs
+++ src/SimpleRandomizer/SimpleRandomizer.cs
@@ -4,6 +4,8 @@ using GREhigh.Infrastructure.Interfaces;
 namespace GREhigh.Infratructure.SimpleRandomizer {
     public class SimpleRandomizer : IRandomizer {
         private static readonly Random _random = new();
-
+        public int NextInt(int min, int max) {
+            return _random.Next(min, max);
+        }
     }
 }
